9c92704665c51ee88b3d15e64b657f15b68abfa5
Troy Kelly <t@aicial.com>
2017-04-02 15:00:10 -0700

    Public release

--- README.md
+++ README.md
@@ -1,2 +1,149 @@
-# reallySecure
-ExpressJS Security Middleware
+# ExpressJS Security Layer
+
+Introduces comprehensive security for your ExpressJS based application.
+
+## Is this for you?
+
+This package has been built for our specific needs. Your mileage may vary. Be very cautious enabling HSTS - do not turn it on unless you understand the implications for your domain and subdomains.
+
+### Prerequisites
+
+This package is called while instantiating your ExpressJS based application. It is not a stand-alone package.
+
+### Installing
+
+Assuming you are starting an ExpressJS project from scratch.
+Create your ExpressJS project.
+
+```
+express --view=hbs --css=less --git my-secure-project
+```
+
+Then install ExpressJS
+
+```
+cd my-secure-project && npm install
+```
+
+Then install this package
+
+```
+npm install --save @really/really-secure
+```
+
+Now you should be able to add it to your application layer
+
+## Using with ExpressJS
+
+In your app.js require the package
+
+```
+var securityLayer = require('really-secure');
+```
+
+Prepare your configuration
+
+```
+var reallySecureConfig = {
+  "csp": {
+    "fontSrc": ["'self'", "use.typekit.net"],
+    "imgSrc": ["'self'", "data:"],
+    "defaultSrc": ["'self'"],
+    "reportUri": "/cspreport",
+    "upgradeInsecureRequests": true
+  },
+  "poweredBy": "A Secure Engine"
+}
+```
+
+Now add it to the application flow
+
+```
+// Secure site
+app.use(securityLayer(reallySecureConfig));
+```
+
+### Example app.js
+
+Your app.js may end up looking like this
+
+```
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var securityLayer = require('really-secure');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+var reallySecureConfig = {
+  "csp": {
+    "fontSrc": ["'self'", "use.typekit.net"],
+    "imgSrc": ["'self'", "data:"],
+    "defaultSrc": ["'self'"],
+    "reportUri": "/cspreport",
+    "upgradeInsecureRequests": true
+  },
+  "poweredBy": "A Secure Engine"
+}
+
+// Secure site
+app.use(securityLayer(reallySecureConfig));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+```
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+
+## Authors
+
+* **Troy Kelly** - *Initial work* - [troykelly](https://github.com/troykelly)
+
+See also the list of [contributors](https://github.com/reallyreally/reallySecure/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the Apache-2.0 License
--- index.js
+++ index.js
@@ -6,127 +6,139 @@ var csp = require('helmet-csp');
 var uuid = require('node-uuid');
 var parseDomain = require("parse-domain");
 
-module.exports = function reallySecure (options) {
-  options = options || {}
+module.exports = function reallySecure(options) {
+	options = options || {}
 
-  return function reallySecure(req, res, next) {
+	return function reallySecure(req, res, next) {
 
-    var makeSecure = function(){
-      res.locals.nonce = uuid.v4();
-      // Sets "X-DNS-Prefetch-Control: on".
-      helmet.dnsPrefetchControl({ allow: true })(req, res, function(){
-        // Don't allow me to be in ANY frames.
-        // Sets "X-Frame-Options: DENY".
-        helmet.frameguard({ action: 'deny' })(req, res, function(){
-          // Set X-Powered-By to Aicial Engine
-          helmet.hidePoweredBy({ setTo: options.poweredBy || 'The Internet' })(req, res, function(){
-            // Sets "X-Download-Options: noopen".
-            helmet.ieNoOpen()(req, res, function(){
-              // Sets "X-Content-Type-Options: nosniff".
-              helmet.noSniff()(req, res, function(){
-                // Sets "Referrer-Policy: same-origin".
-                helmet.referrerPolicy({ policy: 'origin-when-cross-origin' })(req, res, function(){
-                  // Sets "X-XSS-Protection: 1; mode=block".
-                  helmet.xssFilter()(req, res, function(){
-                    if(options.csp === undefined) options.csp = {};
-                    var defaultSrc = options.csp.defaultSrc || ["'self'"];
-                    var scriptSrc = options.csp.scriptSrc || ["'self'"];
-                    var styleSrc = options.csp.styleSrc || ["'self'"];
-                    var fontSrc = options.csp.fontSrc || ["'self'"];
-                    var imgSrc = options.csp.imgSrc || ["'self'"];
-                    var upgradeInsecureRequests = options.csp.upgradeInsecureRequests || true;
+		var makeSecure = function() {
+			res.locals.nonce = uuid.v4();
+			// Sets "X-DNS-Prefetch-Control: on".
+			helmet.dnsPrefetchControl({
+				allow: true
+			})(req, res, function() {
+				// Don't allow me to be in ANY frames.
+				// Sets "X-Frame-Options: DENY".
+				helmet.frameguard({
+					action: 'deny'
+				})(req, res, function() {
+					// Set X-Powered-By to Aicial Engine
+					helmet.hidePoweredBy({
+						setTo: options.poweredBy || 'The Internet'
+					})(req, res, function() {
+						// Sets "X-Download-Options: noopen".
+						helmet.ieNoOpen()(req, res, function() {
+							// Sets "X-Content-Type-Options: nosniff".
+							helmet.noSniff()(req, res, function() {
+								// Sets "Referrer-Policy: same-origin".
+								helmet.referrerPolicy({
+									policy: 'origin-when-cross-origin'
+								})(req, res, function() {
+									// Sets "X-XSS-Protection: 1; mode=block".
+									helmet.xssFilter()(req, res, function() {
+										if (options.csp === undefined) options.csp = {};
+										var defaultSrc = options.csp.defaultSrc || ["'self'"];
+										var scriptSrc = options.csp.scriptSrc || ["'self'"];
+										var styleSrc = options.csp.styleSrc || ["'self'"];
+										var fontSrc = options.csp.fontSrc || ["'self'"];
+										var imgSrc = options.csp.imgSrc || ["'self'"];
+										var upgradeInsecureRequests = options.csp.upgradeInsecureRequests || true;
 
-                    var hostnameParts = parseDomain(req.hostname);
-                    if(hostnameParts) {
-                      var hostname = hostnameParts.domain + '.' + hostnameParts.tld;
-                    } else {
-                      var hostname = req.hostname;
-                    }
+										var hostnameParts = parseDomain(req.hostname);
+										if (hostnameParts) {
+											var hostname = hostnameParts.domain + '.' + hostnameParts.tld;
+										} else {
+											var hostname = req.hostname;
+										}
 
-                    var noSubs = ['localhost', 'appspot.com'];
+										var noSubs = ['localhost', 'appspot.com'];
 
-                    if(!noSubs.includes(hostname)) {
-                      if(defaultSrc.indexOf("api." + hostname) === -1) defaultSrc.push("api." + hostname);
-                      if(scriptSrc.indexOf("script." + hostname) === -1) scriptSrc.push("script." + hostname);
-                      if(styleSrc.indexOf("sytle." + hostname) === -1) styleSrc.push("sytle." + hostname);
-                      if(fontSrc.indexOf("font." + hostname) === -1) fontSrc.push("font." + hostname);
-                      if(imgSrc.indexOf("img." + hostname) === -1) imgSrc.push("img." + hostname);
-                    } else {
-                      upgradeInsecureRequests = false;
-                    }
+										if (!noSubs.includes(hostname)) {
+											if (defaultSrc.indexOf("api." + hostname) === -1) defaultSrc.push("api." + hostname);
+											if (scriptSrc.indexOf("script." + hostname) === -1) scriptSrc.push("script." + hostname);
+											if (styleSrc.indexOf("sytle." + hostname) === -1) styleSrc.push("sytle." + hostname);
+											if (fontSrc.indexOf("font." + hostname) === -1) fontSrc.push("font." + hostname);
+											if (imgSrc.indexOf("img." + hostname) === -1) imgSrc.push("img." + hostname);
+										}
 
-                    var cspConfig = {
-                      // Specify directives as normal.
-                      directives: {
-                        defaultSrc: defaultSrc,
-                        scriptSrc: scriptSrc,
-                        styleSrc: styleSrc,
-                        fontSrc: fontSrc,
-                        imgSrc: imgSrc,
-                        objectSrc: options.csp.objectSrc || ["'none'"]
-                      },
+										if (hostname === 'localhost') {
+											upgradeInsecureRequests = false;
+										}
 
-                      sandbox: options.csp.sandbox || ['allow-forms', 'allow-scripts'],
-                      reportUri: options.csp.reportUri || '/report-violation',
+										var cspConfig = {
+											// Specify directives as normal.
+											directives: {
+												defaultSrc: defaultSrc,
+												scriptSrc: scriptSrc,
+												styleSrc: styleSrc,
+												fontSrc: fontSrc,
+												imgSrc: imgSrc,
+												objectSrc: options.csp.objectSrc || ["'none'"]
+											},
 
-                      // This module will detect common mistakes in your directives and throw errors
-                      // if it finds any. To disable this, enable "loose mode".
-                      loose: false,
+											sandbox: options.csp.sandbox || ['allow-forms', 'allow-scripts'],
+											reportUri: options.csp.reportUri || '/report-violation',
 
-                      // Set to true if you only want browsers to report errors, not block them.
-                      // You may also set this to a function(req, res) in order to decide dynamically
-                      // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
-                      reportOnly: false,
+											// This module will detect common mistakes in your directives and throw errors
+											// if it finds any. To disable this, enable "loose mode".
+											loose: false,
 
-                      // Set to true if you want to blindly set all headers: Content-Security-Policy,
-                      // X-WebKit-CSP, and X-Content-Security-Policy.
-                      setAllHeaders: false,
+											// Set to true if you only want browsers to report errors, not block them.
+											// You may also set this to a function(req, res) in order to decide dynamically
+											// whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+											reportOnly: false,
 
-                      // Set to true if you want to disable CSP on Android where it can be buggy.
-                      disableAndroid: false,
+											// Set to true if you want to blindly set all headers: Content-Security-Policy,
+											// X-WebKit-CSP, and X-Content-Security-Policy.
+											setAllHeaders: false,
 
-                      // Set to false if you want to completely disable any user-agent sniffing.
-                      // This may make the headers less compatible but it will be much faster.
-                      // This defaults to `true`.
-                      browserSniff: true
-                    };
+											// Set to true if you want to disable CSP on Android where it can be buggy.
+											disableAndroid: false,
 
-                    if(upgradeInsecureRequests) {
-                      cspConfig.directives.upgradeInsecureRequests = true
-                    }
+											// Set to false if you want to completely disable any user-agent sniffing.
+											// This may make the headers less compatible but it will be much faster.
+											// This defaults to `true`.
+											browserSniff: true
+										};
 
-                    csp(cspConfig)(req, res, next);
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    }
+										if (upgradeInsecureRequests) {
+											cspConfig.directives.upgradeInsecureRequests = true
+										}
 
-    //When in production make sure we force SSL and send HSTS headers
-    if(process.env.NODE_ENV === 'production') {
-      var enforceHTTPS = function(){
-        enforce.HTTPS({ trustProtoHeader: true })(req, res, makeSecure);
-      }
-      if(options.hsts !== undefined) {
-        // Add HSTS Strict-Transport-Security headers
-        helmet.hsts({
-          maxAge: options.hsts.maxAge || 10886400,        // Must be at least 18 weeks to be approved by Google
-          includeSubDomains: options.hsts.includeSubDomains || true, // Must be enabled to be approved by Google
-          preload: options.hsts.preload || true,
-          force: options.hsts.force || true
-        })(req, res, function(){
-          enforceHTTPS();
-        });
-      } else {
-        enforceHTTPS();
-      }
-    } else {
-      makeSecure();
-    }
+										csp(cspConfig)(req, res, next);
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+		}
 
-  }
+		//When in production make sure we force SSL and send HSTS headers
+		if (process.env.NODE_ENV === 'production') {
+			var enforceHTTPS = function() {
+				enforce.HTTPS({
+					trustProtoHeader: true
+				})(req, res, makeSecure);
+			}
+			if (options.hsts !== undefined) {
+				// Add HSTS Strict-Transport-Security headers
+				helmet.hsts({
+					maxAge: options.hsts.maxAge || 10886400, // Must be at least 18 weeks to be approved by Google
+					includeSubDomains: options.hsts.includeSubDomains || true, // Must be enabled to be approved by Google
+					preload: options.hsts.preload || true,
+					force: options.hsts.force || true
+				})(req, res, function() {
+					enforceHTTPS();
+				});
+			} else {
+				enforceHTTPS();
+			}
+		} else {
+			makeSecure();
+		}
+
+	}
 
 }
