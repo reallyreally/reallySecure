74c30d687f0f6ddaff2e611b8fa5056afe69b6a2
Troy Kelly <troy@troykelly.com>
2018-05-29 07:49:53 -0700

    Cleaned up test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+/*jslint node: true */
+/*jslint esversion: 6 */
 'use strict';
 
 var securityLayer = require('../index');
@@ -5,43 +7,43 @@ var securityLayer = require('../index');
 var securityLayerOptions = require('./test001');
 
 var dummyExpressReq = {
-	hostname: "www.really.ai",
-	headers: {
-		"x-forwarded-proto": "https"
-	}
+  hostname: "www.really.ai",
+  headers: {
+    "x-forwarded-proto": "https"
+  }
 };
 
 var requestStatus = 200;
 
-var dummyExpressRes = function() {}
+var dummyExpressRes = function() {};
 
 dummyExpressRes.locals = {};
 
 dummyExpressRes.setHeader = function(headerToSet, headerValue) {
-	console.log(headerToSet, headerValue);
-	dummyExpressReq[headerToSet] = headerValue;
+  console.log(headerToSet, headerValue);
+  dummyExpressReq[headerToSet] = headerValue;
 };
 
 dummyExpressRes.status = function(statusValue) {
-	console.log(statusValue);
-	requestStatus = statusValue;
+  console.log(statusValue);
+  requestStatus = statusValue;
 };
 
 dummyExpressRes.send = function(sendText) {
-	console.log("Sending:" + sendText);
-}
+  console.log("Sending:" + sendText);
+};
 
 var dummyExpressNext = function() {
-	console.log("Finished");
-}
+  console.log("Finished");
+};
 
 var currentEnv = process.env.NODE_ENV;
 
 process.env.NODE_ENV = "production";
 
 try {
-	var securityLayerTest = securityLayer(securityLayerOptions);
-	securityLayerTest(dummyExpressReq, dummyExpressRes, dummyExpressNext);
+  var securityLayerTest = securityLayer(securityLayerOptions);
+  securityLayerTest(dummyExpressReq, dummyExpressRes, dummyExpressNext);
 } catch (err) {
-	console.log(err);
+  console.log(err);
 }
